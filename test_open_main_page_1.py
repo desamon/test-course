e1b526626344387115d3b3dbf625e4c21e9863c2
Tamara Klochkova <Tamara.Klochkova@data3.ru>
2021-08-16 17:51:06 +0300

    rechanged file

--- test_open_main_page_1.py
+++ test_open_main_page_1.py
@@ -1,2 +1,8 @@
+from selenium import webdriver
 
+def test_open_main_page():
+    driver = webdriver.Chrome()
+    driver.get("https://qastand.valhalla.pw/")
+    print(f'{driver.current_url} is open')
+    driver.quit()
 
