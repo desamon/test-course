5b9dd7c334ae17bc8d863c34feddd7c738946a07
Tamara Klochkova <Tamara.Klochkova@data3.ru>
2021-08-16 18:06:09 +0300

    hw 1

---
+++ test_open_main_page.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+def test_open_main_page():
+    driver = webdriver.Chrome()
+    driver.get('https://qastand.valhalla.pw/')
+    print(f'{driver.current_url} is open')
+    driver.close()
