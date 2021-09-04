48717effca7d91b9d27d508c2de90ecf21d172d0
Tamara Klochkova <Tamara.Klochkova@data3.ru>
2021-09-04 22:20:40 +0300

    rechanged file

--- test_open_main_page.py
+++ test_open_main_page.py
@@ -1,7 +1,7 @@
-from selenium import webdriver
+from selenium.webdriver import Chrome
+
 
 def test_open_main_page():
-    driver = webdriver.Chrome()
-    driver.get('https://qastand.valhalla.pw/')
-    print(f'{driver.current_url} is open')
-    driver.close()
+    with Chrome() as driver:
+        driver.get('https://qastand.valhalla.pw/')
+        print(f'{driver.current_url} is open')
