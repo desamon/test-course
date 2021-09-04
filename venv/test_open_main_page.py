5ff4db598163cee06043a4157e892e2e2430e9a4
Tamara Klochkova <Tamara.Klochkova@data3.ru>
2021-09-04 22:14:30 +0300

    исправила 1 дз

--- venv/test_open_main_page.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver import Chrome
-
-
-def test_open_main_page():
-    with Chrome() as driver:
-        driver.get('https://qastand.valhalla.pw/')
-        print(f'{driver.current_url} is open')
