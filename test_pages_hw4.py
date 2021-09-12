33bdab28a04338b3689a32aecbeb5fa2bf4d9e8d
Tamara Klochkova <Tamara.Klochkova@data3.ru>
2021-09-12 10:22:01 +0300

    Внесла правки по комментариям Полины.

--- test_pages_hw4.py
+++ test_pages_hw4.py
@@ -12,17 +12,18 @@ def test_about_myself_page():
        browser.maximize_window()
        login(browser)
        placeholder_name = browser.find_element_by_name("name").send_keys("Имя")
-       placeholder = browser.find_element_by_name("surname").send_keys("Фамилия")
-       att_manual = browser.find_element(By.ID, "age1").get_attribute("checked") #проверить, что радиобаттон по умол. вкл.
-       att_auto = browser.find_element(By.ID, "age2").click()
-       checkbox_1 = browser.find_element(By.ID, "lang1").click() #выбираем Python
-       checkbox_2 = browser.find_element(By.ID, "lang4").click() #выбираем Go
-       element = browser.find_element(By.CLASS_NAME, "select")
-       Select(browser.find_element_by_tag_name("select")).select_by_index(4) #выбираем Senior
+       browser.find_element_by_name("surname").send_keys("Фамилия")
+       assert browser.find_element(By.ID, "age1").get_attribute("checked"), "Радиобаттон по умолчанию не включен"
+       browser.find_element(By.ID, "age2").click()
+       browser.find_element(By.ID, "lang1").click() #выбираем Python
+       browser.find_element(By.ID, "lang4").click() #выбираем Go
+       Select(browser.find_element_by_tag_name("select")).select_by_index(4)  # выбираем Senior
        placeholder_name = browser.find_element_by_name("name").send_keys(Keys.ENTER) #ENTER в "Имя"
-       text = browser.find_element(By.CSS_SELECTOR, '.is-success').text
 
        assert element_is_present(browser, By.CSS_SELECTOR, '.is-success'), "Необходимый текст отсутствует"
+
+       text = browser.find_element(By.CSS_SELECTOR, '.is-success').text
+
        assert text == "Успех.", f"Неверный текст: {text}"
 
 
@@ -34,9 +35,9 @@ def test_upload_file():
         element_for_upload = browser.find_element(By.CSS_SELECTOR, '[type="file"]')
         element_for_upload.send_keys(os.path.join(os.getcwd(), 'resources', 'koala.jpg'))
         browser.find_element(By.CLASS_NAME, "button").click()
+        assert element_is_present(browser, By.CSS_SELECTOR, '.is-success'), "Необходимый текст отсутствует"
         text = browser.find_element(By.CSS_SELECTOR, '.is-success').text
 
-        assert element_is_present(browser, By.CSS_SELECTOR, '.is-success'), "Необходимый текст отсутствует"
         assert text == "Успех", f"Неверный текст: {text}"
         browser.refresh()
         assert not element_is_present(browser, By.CSS_SELECTOR, '.is-success'), "Необходимый текст отсутствует"
